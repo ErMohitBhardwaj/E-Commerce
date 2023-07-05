0a51dba4a62de872c8a3261640c029a47de25554
ermohitsharma <mohitsharma127308@gmail.com>
2023-07-05 11:32:05 +0530

    Global exception handler class implemented

---
+++ src/main/java/com/bikkadit/ecommerce/exception/GlobalExceptionHandle.java
@@ -0,0 +1,19 @@
+package com.bikkadit.ecommerce.exception;
+
+import com.bikkadit.ecommerce.dto.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandle {
+@ExceptionHandler(ResourceNotFoundException.class)
+ResponseEntity<ApiResponse> resourceNotFoundExceptionHandler(ResourceNotFoundException ex) {
+    ApiResponse response = new ApiResponse();
+    response.setMessage(ex.getMessage());
+    response.setCode(HttpStatus.NOT_FOUND);
+    response.setStatus(false);
+    return new ResponseEntity<ApiResponse>(response,HttpStatus.NOT_FOUND);
+}
+}
