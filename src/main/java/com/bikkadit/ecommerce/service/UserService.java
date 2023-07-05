46db50e11ceb766b3cd95deadb33dd673d7f2df4
ermohitsharma <mohitsharma127308@gmail.com>
2023-07-05 11:24:36 +0530

    User Service Interface created

--- src/main/java/com/bikkadit/ecommerce/service/UserService.java
+++ src/main/java/com/bikkadit/ecommerce/service/UserService.java
@@ -1,5 +1,25 @@
 package com.bikkadit.ecommerce.service;
 
+import com.bikkadit.ecommerce.dto.UserDto;
+
+import java.util.List;
+
 public interface UserService {
+
+    UserDto createUser (UserDto userDto);
+
+    UserDto updateUser(String email, UserDto userDto);
+
+    UserDto getUserById(String id);
+
+    UserDto getUserByEmail(String email);
+
+    UserDto getUserByUsernamePassword(String userName, String password);
+
+    List<UserDto> getAllUsers();
+
+    boolean deleteUserByEmail(String email);
+
+
 
 }
