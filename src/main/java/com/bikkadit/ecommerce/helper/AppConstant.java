233c513fe13e89998bb212343258530fdd7a07b2
ermohitsharma <mohitsharma127308@gmail.com>
2023-07-07 20:56:58 +0530

    Api Response class created and implemented in user module

---
+++ src/main/java/com/bikkadit/ecommerce/helper/AppConstant.java
@@ -0,0 +1,6 @@
+package com.bikkadit.ecommerce.helper;
+
+public class AppConstant {
+    public final static String USER_NOT_FOUND= "User not found with ";
+    public final static String USER_DELETED="User Deleted Successfully.";
+}
