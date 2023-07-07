fa4990aa92db304b65e67379939936796ee5abf3
ermohitsharma <mohitsharma127308@gmail.com>
2023-07-07 20:55:37 +0530

    Validation implemented.

--- src/main/java/com/bikkadit/ecommerce/dto/UserDto.java
+++ src/main/java/com/bikkadit/ecommerce/dto/UserDto.java
@@ -2,6 +2,10 @@ package com.bikkadit.ecommerce.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -10,11 +14,15 @@ import lombok.*;
 public class UserDto {
 
     private String UserId;
-
+    @Size(min=3, max=30, message = "Name Must be contains Minimum 3 or Maximum 30 characters.")
     private String name;
-
+    @Email(message = "Please enter a valid email id.")
     private String email;
+    @Size(min=8, max=20, message = "Password Must be Minimum 8 or Maximum 20 Character.")
     private String password;
+    @Size(min=4, max = 6)
     private String gender;
+    @Size(min=1, max=1000, message = "Maximum length may be 1000 characters.")
     private String about;
+    private String imageName;
 }
